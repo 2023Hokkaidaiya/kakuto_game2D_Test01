37667c0776f15984fa462b5a607e4f8b91eb2b77
2023Hokkaidaiya <122017629+2023Hokkaidaiya@users.noreply.github.com>
2023-06-18 20:30:43 +0900

    更新618

--- Assets/Chara/PlayerController.cs
+++ Assets/Chara/PlayerController.cs
@@ -18,12 +18,19 @@ public class PlayerController : MonoBehaviour
     private float VELOCITY = 2.5f;
 
     //ジャンプ量（定数）
-    private float JUMPPOWER = 4.0f;
+    private float JUMPPOWER = 6.0f;
 
     //地面
     private bool isGround = false;
+
     //着地できるレイヤー
     public LayerMask groundLayer;
+
+    //移動可能
+    private bool isRun = true;
+
+    //攻撃オブジェクト
+    private GameObject attackObject;
 
     // Start is called before the first frame update
     void Start()
@@ -33,54 +40,81 @@ public class PlayerController : MonoBehaviour
 
         //Rigidbodyコンポーネントを取得
         this.myRigidbody = GetComponent<Rigidbody2D>();
+
+        //子オブジェクトを取得
+        this.attackObject = transform.Find("Attack").gameObject;
+
+        //衝突をオフ
+        this.attackObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        //移動
-        if (Input.GetKey(KeyCode.LeftArrow))
+        //移動が可能
+        if (isRun)
         {
-            //ジャンプ
-            if (Input.GetKey(KeyCode.Space) && isGround)
+            //移動
+            if (Input.GetKey(KeyCode.LeftArrow))
             {
-                //左移動＋ジャンプ
-                this.myRigidbody.velocity = new Vector2(-VELOCITY, JUMPPOWER);
+                //ジャンプ
+                if (Input.GetKey(KeyCode.Space) && isGround)
+                {
+                    //左移動＋ジャンプ
+                    this.myRigidbody.velocity = new Vector2(-VELOCITY, JUMPPOWER);
+                }
+                else
+                {
+                    myAnimator.SetInteger("Run", -1);
+
+                    //左移動
+                    this.myRigidbody.velocity = new Vector2(-VELOCITY, this.myRigidbody.velocity.y);
+                }
+            }
+            else if (Input.GetKey(KeyCode.RightArrow))
+            {
+                //ジャンプ
+                if (Input.GetKey(KeyCode.Space) && isGround)
+                {
+                    //右移動＋ジャンプ
+                    this.myRigidbody.velocity = new Vector2(VELOCITY, JUMPPOWER);
+                }
+                else
+                {
+                    myAnimator.SetInteger("Run", 1);
+
+                    //右移動
+                    this.myRigidbody.velocity = new Vector2(VELOCITY, this.myRigidbody.velocity.y);
+                }
             }
             else
             {
-                myAnimator.SetBool("Run", true);
+                myAnimator.SetInteger("Run", 0);
 
-                //左移動
-                this.myRigidbody.velocity = new Vector2(-VELOCITY, this.myRigidbody.velocity.y);
-            }
-        }
-        else if (Input.GetKey(KeyCode.RightArrow))
-        {
-            //ジャンプ
-            if (Input.GetKey(KeyCode.Space) && isGround)
-            {
-                //右移動＋ジャンプ
-                this.myRigidbody.velocity = new Vector2(VELOCITY, JUMPPOWER);
-            }
-            else
-            {
-                myAnimator.SetBool("Run", true);
+                //停止
+                this.myRigidbody.velocity = new Vector2(0f, this.myRigidbody.velocity.y);
 
-                //右移動
-                this.myRigidbody.velocity = new Vector2(VELOCITY, this.myRigidbody.velocity.y);
+                //ジャンプ
+                if (Input.GetKey(KeyCode.Space) && isGround)
+                {
+                    //垂直ジャンプ
+                    this.myRigidbody.velocity = new Vector2(0.0f, JUMPPOWER);
+                }
             }
         }
         else
         {
-            myAnimator.SetBool("Run", false);
+            //停止
+            this.myRigidbody.velocity = new Vector2(0f, this.myRigidbody.velocity.y);
+        }
 
-            //ジャンプ
-            if (Input.GetKey(KeyCode.Space) && isGround)
-            {
-                //垂直ジャンプ
-                this.myRigidbody.velocity = new Vector2(0.0f, JUMPPOWER);
-            }
+        //攻撃
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            //動けなくする
+            isRun = false;
+
+            myAnimator.SetTrigger("PreAttack");
         }
 
         //Debug.Log("velocity: " + this.myRigidbody.velocity.y);
@@ -94,6 +128,27 @@ public class PlayerController : MonoBehaviour
         //this.GetComponent<BoxCollider2D>().enabled = true;
     }
 
+    public void PreAttackStart()
+    {
+        Debug.Log("PreAttackStart");
+    }
+
+    public void AttackStart()
+    {
+        //衝突をオン
+        this.attackObject.SetActive(true);
+        Debug.Log("AttackStart");
+    }
+
+    public void AttackEnd()
+    {
+        //衝突をオフ
+        this.attackObject.SetActive(false);
+        isRun = true;
+        Debug.Log("AttackEnd");
+    }
+
+    /*
     private void FixedUpdate()
     {
         //地上判定
@@ -103,6 +158,36 @@ public class PlayerController : MonoBehaviour
             //地面上OR速度が０ではない
             //速度を更新する
             rbody.velocity = new Vector2(VELOCITY * axisH,rbody.velocity.y);
+        }
+    }
+    */
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            isGround = true;
+        }
+
+        if (other.gameObject.tag == "Attack")
+        {
+            myAnimator.SetTrigger("Damage");
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            isGround = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            isGround = false;
         }
     }
 }
