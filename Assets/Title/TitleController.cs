349b24b17753a6ecd9f77ca78e6af4e7ddf84590
2023Hokkaidaiya <122017629+2023Hokkaidaiya@users.noreply.github.com>
2023-06-27 15:26:31 +0900

    6/27Ver2
    更新

--- Assets/Title/TitleController.cs
+++ Assets/Title/TitleController.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class TitleController : MonoBehaviour
 {
+    static public int assign1Attack = 0;　//UIで値を変更させる。
+    static public int assign2Attack = 0;　//UIで値を変更させる。
+    public string sceneName; //読み込むシーン名
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +21,12 @@ public class TitleController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    //スタートボタンでシーン遷移
+    public void StartButton()
+    {
+        //シーンを読み込む
+        SceneManager.LoadScene(sceneName);
     }
 }
