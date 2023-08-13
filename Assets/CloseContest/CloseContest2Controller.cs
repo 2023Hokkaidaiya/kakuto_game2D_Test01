9c4b452ab7c417ecd7c083fe540405798ece595f
2023Hokkaidaiya <122017629+2023Hokkaidaiya@users.noreply.github.com>
2023-08-13 23:05:07 +0900

    ８/13Ver2

--- Assets/CloseContest/CloseContest2Controller.cs
+++ Assets/CloseContest/CloseContest2Controller.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CloseContestController : MonoBehaviour
+public class CloseContest2Controller : MonoBehaviour
 {
     //Rigidbody2Dïœêî
     //Rigidbody2D rbody;
